9f0730ccf0608b844722aac42a5400b3e4bedffd
Niko Herbias <78730-22256176@SERVER2.DOMAIN>
2024-03-02 13:19:16 +0800

    Footer

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} IT-SYSARCH31-78722-NIKO HERBIAS</p>
+        </footer>
+    );
+}
+
+export default Footer
